27797794ce46013e1f691903c45813fc89e18547
Ivan <ivanitit3535@gmail.com>
2026-03-29 14:11:53 +0300

    fix: server

--- api/search.js
+++ api/search.js
@@ -1,4 +1,4 @@
-import { sendProxyRequest } from './_lib/proxy';
+import { sendProxyRequest } from './_lib/proxy.js';
 
 export default async function handler(req, res) {
   const { q } = req.query;
